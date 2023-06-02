86d98ae5a972661577114d798a3ca9c9cbaca3fe
David Gumberg <davidzgumberg@gmail.com>
2023-06-02 11:21:25 -0400

    Add support for dynamic turbo stream nested form

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -64,6 +64,13 @@ class WorksController < ApplicationController
     end
   end
 
+  def add_author
+    @work = Work.new(work_params.merge({ id: params[:id] }))
+    @work.voices.build.build_author
+
+    render :new
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_work
