c3c2792d23d8141773cdc4568d4a44c80f811134
David Gumberg <davidzgumberg@gmail.com>
2023-07-18 21:44:13 -0400

    Remove extraneous file and bump deps

--- app/models/validators/date_validator.rb
+++
@@ -1,7 +0,0 @@
-class IsNotFutureValidator < ActiveModel::EachValidator
-  def validate_each(record, attribute, value)
-    unless value <= Time.now
-      record.errors.add attribute, (options[:message] || 'must not be in the future')
-    end
-  end
-end
