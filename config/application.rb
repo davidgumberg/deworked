30ffde997d9a83c8b08b4b1cc04b1fcd308df9d0
David Gumberg <davidzgumberg@gmail.com>
2023-09-27 00:27:37 -0400

    Enable experimental ViewComponent monkey patch to fix rendering helpers twice

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,10 @@ module Bookstore
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    # Fixes helper double rendering issues with ViewComponent
+    # May be dangerous!!!
+    config.view_component.capture_compatibility_patch_enabled = true
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
