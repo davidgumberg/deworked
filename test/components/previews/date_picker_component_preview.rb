e3b9a424ff810cc52828ee0acabaac79069e503c
David Gumberg <davidzgumberg@gmail.com>
2023-09-29 09:51:39 -0400

    Add component preview for DatePickerComponent

--- test/components/previews/date_picker_component_preview.rb
+++ test/components/previews/date_picker_component_preview.rb
@@ -2,6 +2,10 @@
 
 class DatePickerComponentPreview < ViewComponent::Preview
   def default
-    render(DatePickerComponent.new(form: "form", date_attr: "date_attr"))
+    work = Work.first
+    object_name = :work
+    view = ActionView::Base.empty
+    form = ActionView::Helpers::FormBuilder.new(object_name, work, view, {})
+    render(DatePickerComponent.new(form:, date_attr: :edition_publication))
   end
 end
