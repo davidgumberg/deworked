d7716f4f7d00ad71aad052460c0d2d09ae464814
David Gumberg <davidzgumberg@gmail.com>
2023-09-29 13:49:27 -0400

    Convert date_picker_component to flexbox

--- test/components/previews/date_picker_component_preview.rb
+++ test/components/previews/date_picker_component_preview.rb
@@ -1,11 +1,12 @@
 # frozen_string_literal: true
 
 class DatePickerComponentPreview < ViewComponent::Preview
-  def default
+  # @param date_attr
+  def default(date_attr: :edition_publication)
     work = Work.first
     object_name = :work
     view = ActionView::Base.empty
     form = ActionView::Helpers::FormBuilder.new(object_name, work, view, {})
-    render(DatePickerComponent.new(form:, date_attr: :edition_publication))
+    render DatePickerComponent.new(form:, date_attr:)
   end
 end
